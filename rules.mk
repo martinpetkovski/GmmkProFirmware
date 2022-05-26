bc5f0e5727aa73c841a45e9646778d01474ae81d
Martin <martinpetkovski@users.noreply.github.com>
2022-05-26 21:49:38 +0200

    Initial submit

---
+++ rules.mk
@@ -0,0 +1,8 @@
+# path: qmk_firmware/keyboards/gmmk/pro/ansi/keymaps/viapro/rules.mk
+VIA_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
+
+IDLE_TIMEOUT_ENABLE = yes
+STARTUP_NUMLOCK_ON = yes
+CONSOLE_ENABLE = yes 
